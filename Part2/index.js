b1338b97a192afd8ffa7cb893474968fcbb3ca2c
dpouls <dallinpoulson@gmail.com>
2019-11-05 22:12:42 -0700

    please work

---
+++ Part2/index.js
@@ -0,0 +1,11 @@
+var idInput = document.getElementById("idInput")
+var colorInput = document.getElementById("colorInput")
+
+function setCard(){
+    var card = document.getElementById(idInput.value)
+    card.style.color = colorInput.value
+    
+}
+
+
+
